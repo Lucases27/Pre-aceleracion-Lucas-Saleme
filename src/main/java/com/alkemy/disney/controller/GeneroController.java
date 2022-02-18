e783f5b1b863554c579977df92c5678ece1e83e2
Lucas Emiliano Saleme <75321699+Lucases27@users.noreply.github.com>
2022-02-18 08:33:57 -0300

    package controller creado

---
+++ src/main/java/com/alkemy/disney/controller/GeneroController.java
@@ -0,0 +1,13 @@
+package com.alkemy.disney.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("continentes")
+public class GeneroController {
+
+	public void save() {
+		
+	}
+}
