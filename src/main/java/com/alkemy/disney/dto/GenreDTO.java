4a2b1defc52cc73c2c3fa06dd47d17bba277ef56
Lucas Emiliano Saleme <75321699+Lucases27@users.noreply.github.com>
2022-02-24 08:22:02 -0300

    error al aplicar javax.validation.valid

--- src/main/java/com/alkemy/disney/dto/GenreDTO.java
+++ src/main/java/com/alkemy/disney/dto/GenreDTO.java
@@ -3,10 +3,13 @@ package com.alkemy.disney.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 public class GenreDTO {
-
 	private Long id;
 	private String name;
 	private String image;
